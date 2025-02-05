9f1a7cda73e70b007aa92874b7d378241fd43d5f
Michael Abrahams <miabraha@gmail.com>
2025-02-04 21:25:42 -0500

    fix etc.py

--- ComfyAutomation/lib/etc.py
+++ ComfyAutomation/lib/etc.py
@@ -1,14 +1,16 @@
+from pathlib import Path
 
 special_tags = ['source_furry', 'source_anime', 'score_9', 'score_8_up', 'score_7_up', 'rating_explicit', 'rating_questionable', 'rating_safe']
 
-
-for tagfile in Path(BASE_PATH).glob(r'**\*.txt'):
-    with tagfile.open('+r') as f:
-        file_tags = f.read().split(',')
-        file_tags = tagfile.open('+r').read().split(',')
-        new_tags = [tag.replace("_", " ") if tag not in special_tags else tag for tag in file_tags]
-        new_tags = ','.join(new_tags)
-        f.close()
-    with tagfile.open('w') as f:
-        f.write(new_tags)
-        f.close()
+# Rewrites tags in all tagfiles in BASE_PATH
+def rewrite_tags(BASE_PATH):
+    for tagfile in Path(BASE_PATH).glob(r'**\*.txt'):
+        with tagfile.open('+r') as f:
+            file_tags = f.read().split(',')
+            file_tags = tagfile.open('+r').read().split(',')
+            new_tags = [tag.replace("_", " ") if tag not in special_tags else tag for tag in file_tags]
+            new_tags = ','.join(new_tags)
+            f.close()
+        with tagfile.open('w') as f:
+            f.write(new_tags)
+            f.close()
